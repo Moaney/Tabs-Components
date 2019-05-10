bccbc8f3e87a3dc4bc750ee1ca771c22de47247d
LaDonna Moaney <ladonnamoney@gmail.com>
2019-05-09 22:38:29 -0400

    dropdown menu toggles, but disapprears when clicked

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,26 +1,27 @@
 class Dropdown {
-  constructor(dropdown) {
+  constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.dropdown = dropdown;
+    this.element = element;
     // console.log(dropdown);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.dropdown.getElementsByClassName('dropdown-button');
+    this.button = document.querySelector('.dropdown-button');
     // console.log(this.button);
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = document.querySelector('.dropdown-content');
     // console.log(this.content);
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => toggleContent());
+    this.button.addEventListener('click', () => this.toggleContent());
   }
 
   toggleContent() {
       // Toggle the ".dropdown-hidden" class off and on
-      this.content.classList.toggle('dropdown-hidden');
+      this.button.classList.toggle('dropdown-hidden');
+      this.content.classList.toggle('dropdown-content');
 
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
